72efb0ab59f28127c1204f75e658febd7e4aaf32
khakiee <khakie@vcnc.co.kr>
2023-12-09 16:27:06 +0900

    impl: houseService skeleton

--- house/src/main/kotlin/xyz/bukinator/house/service/HouseService.kt
+++ house/src/main/kotlin/xyz/bukinator/house/service/HouseService.kt
@@ -1,6 +1,30 @@
 package xyz.bukinator.house.service
 
 import org.springframework.stereotype.Service
+import xyz.bukinator.house.model.House
+import xyz.bukinator.house.repository.HouseRepository
+import java.time.LocalDateTime
+import java.util.UUID
 
 @Service
-class HouseService
+class HouseService(
+    val houseRepository: HouseRepository,
+) {
+    fun get(id: UUID) = houseRepository.findById(id)
+
+    fun list(criteria: HouseQueryCriteria): List<House> = emptyList()
+
+    fun create(house: House): House = houseRepository.save(house)
+
+    fun delete(id: UUID) {
+        get(id).orElse(null)?.let {
+            it.deletedAt = LocalDateTime.now()
+            houseRepository.save(it)
+            // TODO: Define Exception
+        } ?: throw Exception("Not found")
+    }
+
+    data class HouseQueryCriteria(
+        val asdf: String,
+    )
+}
