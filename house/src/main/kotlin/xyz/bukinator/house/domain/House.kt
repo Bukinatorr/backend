83a24364445b994119a6e6dc0b19745c65410b51
khakiee <khakie@vcnc.co.kr>
2023-12-09 15:50:20 +0900

    fix: build error

--- house/src/main/kotlin/xyz/bukinator/house/domain/House.kt
+++ house/src/main/kotlin/xyz/bukinator/house/domain/House.kt
@@ -1,6 +1,7 @@
 package xyz.bukinator.house.domain
 
 import jakarta.persistence.Column
+import jakarta.persistence.Convert
 import jakarta.persistence.Embedded
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
