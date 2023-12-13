e89e6db39d1d9e317ebbfc3d1766082d9d5f8778
minsookoo <minsoo.koo@myrealtrip.com>
2023-12-13 20:32:05 +0900

    temp: 매물 데이터 생성 배치 추가 (가안)

--- batch/build.gradle.kts
+++ batch/build.gradle.kts
@@ -1,5 +1,12 @@
 plugins {
+    id("org.springframework.boot")
 }
 
 dependencies {
+    implementation(project(":house"))
+
+    implementation ("org.springframework.boot:spring-boot-starter-batch")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    testImplementation("org.springframework.batch:spring-batch-test")
 }
---
+++ batch/src/main/kotlin/xyz/bukinator/batch/core/OriginHouseDataCreateConfig.kt
@@ -0,0 +1,98 @@
+package xyz.bukinator.batch.core
+
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.Step
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.JobScope
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepScope
+import org.springframework.batch.core.launch.support.RunIdIncrementer
+import org.springframework.batch.item.ItemProcessor
+import org.springframework.batch.item.ItemWriter
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class OriginHouseDataCreateConfig(
+    val houseService: HouseService,
+    val jobBuilderFactory: JobBuilderFactory,
+    val stepBuilderFactory: StepBuilderFactory,
+    val originHouseDataReader: OriginHouseDataReader,
+) {
+    @Bean(name = ["OriginHouseDataCreateJob"])
+    @Throws(Exception::class)
+    fun OriginHouseDataCreateJob(): Job {
+        return jobBuilderFactory.get("OriginHouseDataCreateJob")
+            .incrementer(RunIdIncrementer())
+            .start(OriginHouseDataCreateStep())
+            .build()
+    }
+
+    @Bean
+    @JobScope
+    @Throws(Exception::class)
+    fun OriginHouseDataCreateStep(): Step {
+        return stepBuilderFactory.get("OriginHouseDataCreateStep")
+            .chunk<OrginHouseRawData, CreateHouseDto>(100)
+            .reader(originHouseDataReader)
+            .processor(OriginHouseDataProcessor())
+            .writer(OriginHouseDataWriter())
+//            .listener() create 과정 중 중복 데이터 발생 listener 추가 필요 -> update로 연계
+            .build()
+    }
+
+    @Bean
+    @StepScope
+    fun OriginHouseDataProcessor(): ItemProcessor<OrginHouseRawData, CreateHouseDto> {
+        return ItemProcessor { rawData ->
+            return CreateHouseDto(
+                originSource = rawData["originSource"],
+                originId = rawData["originId"],
+                originUpdatedAt = rawData["originUpdatedAt"],
+                salesType = rawData["salesType"],
+                houseName = rawData["houseName"],
+                houseType = rawData["houseType"],
+                roomType = rawData["roomType"],
+                roomDirection = rawData["roomDirection"],
+                thumbnail = rawData["thumbnail"],
+                images = rawData["images"],
+                priceDeposit = rawData["priceDeposit"],
+                priceRent = rawData["priceRent"],
+                priceManage = rawData["priceManage"],
+                priceManageIncludes = rawData["priceManageIncludes"],
+                areaContract = rawData["areaContract"],
+                areaSupply = rawData["areaSupply"],
+                areaIndividual = rawData["areaIndividual"],
+                title = rawData["title"],
+                description = rawData["description"],
+                status = rawData["status"],
+                lat = rawData["lat"],
+                lng = rawData["lng"],
+                parkingCount = rawData["parkingCount"],
+                elevator = rawData["elevator"],
+                movinDate = rawData["movinDate"],
+                approveDate = rawData["approveDate"],
+                residenceType = rawData["residenceType"],
+                pnu = rawData["pnu"],
+                floorTotal = rawData["floorTotal"],
+                floorTarget = rawData["floorTarget"],
+                options = rawData["options"],
+                addressLocal1 = rawData["addressLocal1"],
+                addressLocal2 = rawData["addressLocal2"],
+                addressLocal3 = rawData["addressLocal3"],
+                addressLocal4 = rawData["addressLocal4"],
+                addressJibun = rawData["addressJibun"],
+            )
+        }
+    }
+
+    @Bean
+    @StepScope
+    fun OriginHouseDataWriter(): ItemWriter<CreateHouseDto> {
+        return ItemWriter { createHousesDto ->
+            createHousesDto.forEach { createHouseDto ->
+                houseService.createHouse(createHouseDto)
+            }
+        }
+    }
+}
