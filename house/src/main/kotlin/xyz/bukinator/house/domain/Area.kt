096235b48cf713599c2cb6c1cfe8ecf0f4cc873d
minsookoo <minsoo.koo@myrealtrip.com>
2023-12-13 20:21:23 +0900

    feat: house 생성, 수정, 삭제 기능 추가

--- house/src/main/kotlin/xyz/bukinator/house/domain/Area.kt
+++ house/src/main/kotlin/xyz/bukinator/house/domain/Area.kt
@@ -8,15 +8,15 @@ import org.hibernate.annotations.Comment
 class Area(
     @Column(name = "area_contract")
     @Comment("계약 면적")
-    val areaContract: Double?,
+    var areaContract: Double?,
 
     @Column(name = "area_supply")
     @Comment("공급 면적")
-    val areaSupply: Double?,
+    var areaSupply: Double?,
 
     @Column(name = "area_individual")
     @Comment("전용 면적")
-    val areaIndividual: Double?,
+    var areaIndividual: Double?,
 ) {
     init {
         require(areaContract != null || areaSupply != null || areaIndividual != null) {
