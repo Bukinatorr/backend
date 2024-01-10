19860a807796a415e97810221baf29708d11efb6
khakiee <khakie@vcnc.co.kr>
2024-01-10 21:40:56 +0900

    review: remove unused dependency

--- batch/build.gradle.kts
+++ batch/build.gradle.kts
@@ -7,6 +7,4 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-batch")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-
-    testImplementation("org.springframework.batch:spring-batch-test")
 }
